594782150c97c57798e90efd4f86e51dcd722157
Lucas Teodoro <lucasteosan@gmail.com>
2026-08-23 23:01:36 -0300

    Aula 19/08/2026

---
+++ Comunicacao_Entre_Camadas/Controllers/MedicosController.cs
@@ -0,0 +1,39 @@
+﻿using Comunicacao_Entre_Camadas.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Comunicacao_Entre_Camadas.Controllers
+{
+    public class MedicosController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Cria o primeiro objeto do tipo Medico
+            Medico medico1 = new Medico
+            {
+                Id = 1,
+                Nome = "Ana Souza",
+                Crm = "12345",
+                Especialidade = "Cardiologia"
+            };
+
+            // Cria o segundo objeto do tipo Medico
+            Medico medico2 = new Medico
+            {
+                Id = 2,
+                Nome = "Carlos Lima",
+                Crm = "67890",
+                Especialidade = "Ortopedia"
+            };
+
+            // Cria uma lista que armazenará objetos do tipo Medico
+            List<Medico> medicos = new List<Medico>();
+
+            // Adiciona os objetos criados à lista
+            medicos.Add(medico1);
+            medicos.Add(medico2);
+
+            // Envia a lista de médicos para a View
+            return View(medicos);
+        }
+    }
+}
---
+++ Comunicacao_Entre_Camadas/Models/Medico.cs
@@ -0,0 +1,10 @@
+﻿namespace Comunicacao_Entre_Camadas.Models
+{
+    public class Medico
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty; //Esse comando atribui uma string vazia para o atributo 
+        public string Crm { get; set; } = string.Empty;
+        public string Especialidade { get; set; } = string.Empty;
+    }
+}
